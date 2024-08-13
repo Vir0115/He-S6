7f235b04e9959c4180abfd67366092c31c2a4752
hevirginia93 <hevirginia93@gmail.com>
2024-08-13 14:01:53 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+print ("Hola Mundo")
+print ("Bienvenidos a S6") 
+
+print ("Como se llama")
+nombre = "Vir"
+print ("Mucho gusto")
+
+nombre = "Vir"
+edad = 25
+peso = 52
+mayorEdad = True
+menorEdad = False
+
+print (nombre)
+print (edad)
+print (peso)
+print (mayorEdad)
+print (menorEdad) 
+
+""" Codigo 1 """
+print ("Hola, mundo!")
+
+num = 10
+
+print("El número es", num)
+
+suma = 5 + 3
+
+print(suma)
+
+""" Codigo 2 """
+nombre = "Juan"
+
+apellido = "Pérez"
+
+nombre_completo = nombre + apellido
+
+print(nombre_completo)
+
+""" Codigo 3 """
+edad = int(input("¿Cuántos años tienes? "))
+
+print("El año que viene tendrás", edad + 1, "años.")
